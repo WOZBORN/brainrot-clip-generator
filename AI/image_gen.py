76359e422dfe2abfdfdd640b3c4bb5c5951b88b6
gvaysman222 <126403195+gvaysman222@users.noreply.github.com>
2025-05-15 21:30:13 +0300

    abababababa

---
+++ AI/image_gen.py
@@ -0,0 +1,17 @@
+import base64
+from openai import OpenAI
+
+def generate_image(prompt: str, client):
+    resp = client.images.generate(
+        model="gpt-image-1",
+        prompt=prompt,
+        n=1,
+        quality="low",
+        size="1024x1024"
+    )
+    img_bytes = base64.b64decode(resp.data[0].b64_json)
+
+    with open("output.png", "wb") as f:
+        f.write(img_bytes)
+
+    return "output.png"
